1fcecee33690fd69087a12b755ea88701b61b410
IanL73 <ianl73@nycstudents.net>
2025-03-27 09:25:28 -0400

    shit

--- +2_Mult_per_reroll_in_the_shop.py
+++ +2_Mult_per_reroll_in_the_shop.py
@@ -1,16 +1,37 @@
-class User:
-    def __init__(self, email, password):
-        self.password = password
-        self.email = email
-    
+while True:
+    mode = input("What mode? ")
+    if mode == "Student" or mode == "student" or mode == "S" or mode == "s":
+        print("In Student mode.")
+        break
+    elif mode == "Teacher" or mode == "teacher" or mode == "t" or mode == "T":
+        print("In Teacher mode.")
+        break
+    else:
+        print("Invalid input.")
+        print()
+
+class Flash:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
     def display_info(self):
-        return f"User: {self.name}, Email: {self.email}"
+        return f"{self.question} {self.answer}"
     
-class Student(User):
-    def __init__(self, email, password):
-        super().__init__(email, password) # Call the parent class constructor
+    def to_dict(self):
+        return {"Question": self.question, "Answer": self.answer}
     
-class Teacher(User):
-    def __init__(self, email, password, key):
-        super().__init__(email, password)
-        self.key = key
+    def quiz(self):
+        if input(f"{self.question} ") == self.answer:
+            print("Correct!")
+        else:
+            print("Wrong!!")
+
+    def create(self):
+        new_question = input("Input the question for this card. ")
+        new_answer = input("Input the answer for this card. ")
+        new_card = Flash(new_question, new_answer)
+        cards_data = 
+        try:
+            with open("FlashCards.json", "w") as file:
+                cards_data json.load(file)
