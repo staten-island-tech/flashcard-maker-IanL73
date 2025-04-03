f6ec868c2ba7de45fa96ef3bc3b793d6ccc3e1aa
IanL73 <ianl73@nycstudents.net>
2025-04-03 08:52:11 -0400

    piss

--- +2_Mult_per_reroll_in_the_shop.py
+++ +2_Mult_per_reroll_in_the_shop.py
@@ -26,7 +26,9 @@ class Flash:
         return {"Question": self.question, "Answer": self.answer}
 
 if teacher == True:
+
     new_or_add = input("Would you like to create a new card set or add to an old one? ")
+
     if new_or_add == "new" or new_or_add == "New":
         cards = []
         while True:
@@ -39,20 +41,28 @@ if teacher == True:
                 break
         with open("cards.json", "w") as file:
             json.dump(cards, file, indent=4)
+
     elif new_or_add == "add" or new_or_add == "Add":
+
         cards = []
+
         while True:
             question = input("What is the question for this flashcard?")
             answer = input("What is the answer for this flashcard?")
             new_card = Flash(question, answer)
             cards.append(new_card.to_dict())
             cont = input("more card?")
+
             if cont == "no" or cont == "No":
                 break
+
         try:
             with open("cards.json", "r") as file:
                 cards_data = json.load(file)
         except FileNotFoundError:
             cards_data = []
+        
         cards_data.append(cards)
-        with open("cars.json",)
+
+        with open("cards.json", "w") as file:
+            json.dump(cards_data, file, indent=4)
